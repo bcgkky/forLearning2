31e793c3df34eae3118a6c50087f2689c046ae9a
bcgkky <bcgkky1@gmail.com>
2023-12-07 12:09:27 +0300

    fnsh
    ...

---
+++ Assets/agents.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class agents : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject[] hedefler;
+    int mevcuthedef;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        mevcuthedef = 0;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(hedefler[mevcuthedef].transform.position);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (mevcuthedef == 0)
+        {
+            mevcuthedef = 1;
+            agent.SetDestination(hedefler[mevcuthedef].transform.position);
+        }
+        else if (mevcuthedef == 1)
+        {
+            mevcuthedef = 2;
+            agent.SetDestination(hedefler[mevcuthedef].transform.position);
+        }
+        else if (mevcuthedef == 2)
+        {
+            mevcuthedef = 3;
+            agent.SetDestination(hedefler[mevcuthedef].transform.position);
+        }
+
+        else if (mevcuthedef == 3)
+        {
+            mevcuthedef = 4;
+            agent.SetDestination(hedefler[mevcuthedef].transform.position);
+        }
+
+        else if (mevcuthedef == 4)
+        {
+            mevcuthedef = 5;
+            agent.SetDestination(hedefler[mevcuthedef].transform.position);
+        }
+
+        else if (mevcuthedef == 5)
+        {
+            mevcuthedef = 6;
+            agent.SetDestination(hedefler[mevcuthedef].transform.position);
+        }
+
+        else if (mevcuthedef == 6)
+        {
+            mevcuthedef = 7;
+            agent.SetDestination(hedefler[mevcuthedef].transform.position);
+        }
+        else
+        {
+            mevcuthedef = 0;
+            agent.SetDestination(hedefler[mevcuthedef].transform.position);
+        }
+    }
+}
---
+++ Assets/boyat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boyat : MonoBehaviour
+{
+    public Color clr;
+   void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("agent"))
+        {
+            other.gameObject.GetComponent<MeshRenderer>().material.color = clr;
+        }
+
+        /*GameObject obj = GameObject.FindGameObjectWithTag("agent");
+        obj.GetComponent<MeshRenderer>().material.color = Color.green;*/
+
+    }
+}
